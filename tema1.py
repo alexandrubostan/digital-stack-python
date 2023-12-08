e559c1bebc2c40f8b859b8304c376de3219829b0
Alexandru Bostan <alex.bostan007@gmail.com>
2023-12-08 17:44:05 +0200

    completed first task

--- tema1.py
+++ tema1.py
@@ -1,1 +1,12 @@
-myList = []
+myList = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+myListAux = sorted(myList)
+
+print(myListAux)
+
+print(myListAux[::-1])
+
+print(myListAux[1:10:2])
+
+print(myListAux[0:9:2])
+
+print(myListAux[2:9:3])
